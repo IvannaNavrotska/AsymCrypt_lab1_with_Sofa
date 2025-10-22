fda5bbb958239ef2fbf16596df4d19b605b6d584
sofakob <hhitech628@gmail.com>
2025-10-22 17:42:01 +0300

    bm

--- bm.py
+++ bm.py
@@ -6,20 +6,20 @@ def proverka_x(t, p):
     else:
         return 0
     
-def bm(a, p):
+def bm(a, p, k):
     t=random.randint(0, p-1)
-    x=[]
-    x.append(proverka_x(t, p))
     dlinna=32-1
-
-
-    for _ in range(dlinna):
-        t=pow(a, t, p)
+    arr=[]
+    for _ in range(int(k/32)):
+        x=[]
         x.append(proverka_x(t, p))
-    return x
+        for _ in range(dlinna):
+            t=pow(a, t, p)
+            x.append(proverka_x(t, p))
+        arr.append(x)
+    return arr
 
 p=int(0xCEA42B987C44FA642D80AD9F51F10457690DEF10C83D0BC1BCEE12FC3B6093E3)
 a=int(0x5B88C41246790891C095E2878880342E88C79974303BD0400B090FE38A688356)
-c=int("".join(str(x) for x in bm(a, p)), 2)
-#print(int(c))
+#print(bm(a, p, 1000000))
     
--- bmbayts.py
+++ bmbayts.py
@@ -2,20 +2,25 @@ import random
 
 
     
-def bm_bayts(a, p):
+def bm_bayts(a, p, k):
     # на виході список з чисел до 255 включно
     t=random.randint(0, p-1)
     x=[]
-    k=int((256*t)//(p-1))
-    x.append(k)
+    l=int((256*t)//(p-1))
+    x.append(l)
     dlinna=32-1
-
-
-    for _ in range(dlinna):
-        t=pow(a, t, p)
-        k=int((256*t)//(p-1))
-        x.append(k)
-    return x
+    arr=[]
+    for _ in range(int(k/32)):
+        x=[]
+        l=int((256*t)//(p-1))
+        x.append(l)
+        for _ in range(dlinna):
+            t=pow(a, t, p)
+            l=int((256*t)//(p-1))
+            x.append(l)
+        arr.append(x)
+    
+    return 
 
 p=int(0xCEA42B987C44FA642D80AD9F51F10457690DEF10C83D0BC1BCEE12FC3B6093E3)
 a=int(0x5B88C41246790891C095E2878880342E88C79974303BD0400B090FE38A688356)
